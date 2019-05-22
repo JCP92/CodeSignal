c76f0085b9a3a0f5f091c43ea662bf92ea659ccb
Joshua Peckham <joshpeck_11@hotmail.com>
2019-05-21 22:05:27 -0700

    Update add.java

--- TheJourneyBegins/add.java
+++ TheJourneyBegins/add.java
@@ -1,9 +1,3 @@
-boolean checkPalindrome(String inputString) {
-    
-    for(int i = 0, j = inputString.length() - 1; (i != j) && (i < j); i++, j--){
-        if((int)inputString.charAt(i) != (int)inputString.charAt(j)){
-            return false;
-        }
-    }
-    return true;
+int add(int param1, int param2) {
+    return param1 + param2;
 }
